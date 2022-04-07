ec46dd0faa255ef16f96cfef2b208ec1bcd283d1
katy01101011 <katy.91560@gmail.com>
2022-04-07 17:15:29 +0200

    add: ticket-price

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-// Chiedere all'utente quanti chilometri vuole percorrere
+// Qual'è il prezzo del biglietto del treno?
+let ticketPrice = '';
+const humanTicketPrice = ticketPrice;
+console.log(humanTicketPrice);
 
-// Chiedere l'età del passeggero
+// Chiedere all'utente quanti chilometri vuole percorrere
+const passengerDistance = parseInt(prompt('Quanti chilometri vuoi percorrere?'))
+console.log(passengerDistance, typeof(passengerDistance));
 
 // Prezzo biglietto al chilometro
+const regularTicket = passengerDistance * 0.21;
+console.log(regularTicket, typeof(regularTicket));
 
-// Il passeggero non è un maggiorenne con meno di 65 anni?
-    // NO - Il prezzo del biglietto è il prezzo base
+// Chiedere l'età del passeggero
+const passengerAge = parseInt(prompt('Quanti anni ha il passeggero?'));
+console.log(passengerAge, typeof(passengerAge));
 
-    // SI 
+// Il passeggero è un maggiorenne con meno di 65 anni?
+    // SI - Il prezzo del biglietto è il prezzo base
+
+    // NO 
         // Il passeggero ha meno di 18 anni?
             // SI - Il prezzo del biglietto base viene scontato del 20%
             // NO - Il prezzo del biglietto base viene scontato del 40%
+if (passengerAge >= 18 && passengerAge <= 65) {
+    ticketPrice = regularTicket;
+    console.log(ticketPrice);
+} else if (passengerAge < 18) {
+    ticketPrice = ((regularTicket * 80) / 100);
+    console.log(ticketPrice);
+} else {
+    ticketPrice = (regularTicket * 60) / 100;
+    console.log(ticketPrice);
+}
 
-
-
-
-
+document.getElementById("ticket-price").innerHTML = ticketPrice.toFixed(2);
