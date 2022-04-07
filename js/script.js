42d8d6aaadaef1c1b101f9c2888964cc2a1de254
katy01101011 <katy.91560@gmail.com>
2022-04-07 16:19:23 +0200

    flowchart + pseudocodice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert('Ciao!')
+// Chiedere all'utente quanti chilometri vuole percorrere
+
+// Chiedere l'età del passeggero
+
+// Prezzo biglietto al chilometro
+
+// Il passeggero non è un maggiorenne con meno di 65 anni?
+    // NO - Il prezzo del biglietto è il prezzo base
+
+    // SI 
+        // Il passeggero ha meno di 18 anni?
+            // SI - Il prezzo del biglietto base viene scontato del 20%
+            // NO - Il prezzo del biglietto base viene scontato del 40%
+
+
+
+
+
